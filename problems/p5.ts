2903c7f7de1d1167065a5f367b2b04adade53f9e
rab <rablork@gmail.com>
2024-05-24 12:49:59 +0300

    till p6.ts

--- problems/p5.ts
+++ problems/p5.ts
@@ -1,7 +1,32 @@
-import { groupBy, map, reduce, sumBy } from "remeda";
+// import { groupBy, map, reduce, sumBy } from "remeda";
 import { prisma } from "./prisma";
-import { StarRating } from "@prisma/client";
+// import { StarRating } from "@prisma/client";
 
 // hint:find all stars with the movies "included" on, then good ol' javascript should finish the job
 // This one should require more javascript work than the previous ones
-export const getAllMoviesWithAverageScoreOverN = async (n: number) => {};
+export const getAllMoviesWithAverageScoreOverN = async (n: number) => {
+    const movies = await prisma.movie.findMany({
+        select: {
+            id: true,
+            title: true,
+            releaseYear: true,
+            starRatings: true,
+            parentalRating: true,
+        }
+    });
+    const filteredMovies = movies.filter(movie => {
+        if(movie.starRatings.length === 0) 
+            return false;
+        
+        const averageScore = movie.starRatings.reduce((acc, starRating) => acc + starRating.score, 0) / movie.starRatings.length;
+        return averageScore > n;
+    }).map(movie => {
+        return {
+            id: movie.id,
+            title: movie.title,
+            releaseYear: movie.releaseYear,
+            parentalRating: movie.parentalRating
+        }
+    });
+    return filteredMovies;
+};
--- problems/p6.ts
+++ problems/p6.ts
@@ -1,4 +1,18 @@
 import { prisma } from "./prisma";
 
 // find all movies that a user has watched
-export const findAllMoviesThatAUserWatched = async (userId: number) => {};
+export const findAllMoviesThatAUserWatched = async (userId: number) => {
+    const userMovies = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+      select: {
+        starRatings: {
+            select  : {
+                movie: true
+                }
+            }
+        }
+    });
+    return userMovies? userMovies.starRatings.map(starRating => starRating.movie) : [];
+};
