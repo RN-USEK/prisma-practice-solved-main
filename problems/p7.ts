b6d132ce1b307b1d2d1a8a89a12e545ae583f134
rab <rablork@gmail.com>
2024-05-27 10:17:17 +0300

    till p8.ts

--- problems/p7.ts
+++ problems/p7.ts
@@ -1,4 +1,18 @@
 import { prisma } from "./prisma";
 
 // get average score for a user
-export const getAverageScoreForUser = async (userId: number) => {};
+export const getAverageScoreForUser = async (userId: number) => {
+   const userWithRatings = await prisma.user.findUnique({
+        where: {
+            id: userId,
+        },
+        select: {
+            starRatings:{
+                select:{
+                    score:true
+                },
+            },
+        },
+    });
+    return userWithRatings?userWithRatings.starRatings.reduce((acc, curr) => acc + curr.score, 0) / userWithRatings.starRatings.length:0;
+};
--- problems/p8.ts
+++ problems/p8.ts
@@ -1,10 +1,30 @@
 import { maxBy, minBy } from "remeda";
 import { prisma } from "./prisma";
-
 // Always tell truths, don't you ever lie, to solve this problem, just try a `groupBy`
 
 // find the critic with the lowest average score
-export const findTheGrumpiestCriticId = async () => {};
+export const findTheGrumpiestCriticId = async () => {
+    return findCriticId('asc');
+};
 
 // find the critic with the highest average score
-export const findTheNicestCriticId = async () => {};
+export const findTheNicestCriticId = async () => {
+    return findCriticId('desc');
+};
+
+const findCriticId = async (order: 'asc' | 'desc') => {
+    const result = await prisma.starRating.groupBy({
+        by: ['userId'],
+        _avg: {
+            score: true,
+        },
+        orderBy: {
+            _avg: {
+                score: order,
+            },
+        },
+        take: 1,
+    });
+
+    return result.length > 0 ? result[0].userId : null;
+};
